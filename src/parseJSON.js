a47e9c57d86d1c4395c3b1d13643d087fc1945bd
Steven Lundy <steven.lundy@gmail.com>
2015-07-16 08:20:57 -0400

    Fixed throw statements to throw Errors

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -10,42 +10,42 @@ var parseJSON = function(json) {
     if (_.last(json[0]) === '}'){
 
     } else {
-      throw 'Invalid Object';
+      throw new Error('Invalid Object');
     }
   } else if (json[0] === '['){
     // Array
     if (_.last(json[0]) === ']'){
 
     } else {
-      throw 'Invalid Array';
+      throw new Error('Invalid Array');
     }
   } else if (json[0] === '"'){
     // String
     if (_.last(json[0]) === '"'){
 
     } else {
-      throw 'Invalid String';
+      throw new Error('Invalid String');
     }
   } else if (json[0] === 't'){
     // true
     if (json === 'true'){
       return true;
     } else {
-      throw 'Invalid keyword';
+      throw new Error('Invalid keyword');
     }
   } else if (json[0] === 'f'){
     // false
     if (json === 'false'){
       return false;
     } else {
-      throw 'Invalid keyword';
+      throw new Error('Invalid keyword');
     }
   } else if (json[0] === 'n'){
     // null
     if (json === 'null'){
       return null;
     } else {
-      throw 'Invalid keyword';
+      throw new Error('Invalid keyword');
     }
   } else if (/[0-9-]/.test(json[0])){
     // Number
