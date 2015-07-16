ebd4e61fd43053a2f3563034d1499ca3399eeeaf
Steven Lundy <steven.lundy@gmail.com>
2015-07-16 08:04:29 -0400

    Added base cases for parseJSON function

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,28 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+
+  json = json.trim();
+  if (json[0] === '{'){
+    // Object
+
+  } else if (json[0] === '['){
+    // Array
+
+  } else if (json[0] === '"'){
+    // String
+
+  } else if (json[0] === 't'){
+    // true
+
+  } else if (json[0] === 'f'){
+    // false
+
+  } else if (json[0] === 'n'){
+    // null
+
+  } else if (/[0-9-]/.test(json[0])){
+    // Number
+
+  }
 };
