abce5101dff5c07f4d4cac1b4cbabc9b1739ee9b
Steven Lundy <steven.lundy@gmail.com>
2015-07-19 19:21:01 -0400

    Wrote base cases to check what type of variable to parse

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,26 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+
+  var charIndex = 0;
+  var getVariable = function(){
+    if(json[charIndex] === '{'){
+      return getObject();
+    } else if(json[charIndex] === '['){
+      return getArray();
+    } else if(json[charIndex] === '"'){
+      return getString();
+    } else if(json.slice(charIndex, charIndex+4) === 'true'){
+      charIndex += 4;
+      return true;
+    } else if(json.slice(charIndex, charIndex+5) === 'false'){
+      charIndex += 5;
+      return false;
+    } else if(json.slice(charIndex, charIndex+4) === 'null'){
+      charIndex += 4;
+      return null;
+    } else if (/[0-9-]/.test(json[charIndex])){
+      return getNumber();
+    }
+  }
 };
